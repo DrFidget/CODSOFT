39e6ef6abc8611989957e36f812b00df781d863d
Aleena-Tanweer <aleenatanweer2002@gmail.com>
2023-10-02 09:44:54 +0500

    portfolio fixed

--- PORTFOLIO/src/App.tsx
+++ PORTFOLIO/src/App.tsx
@@ -1,6 +1,31 @@
-// import NaveBar from "./components/NaveBar";
-import LandingPage from "./components/LandingPage";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./Components/Main/Main.tsx";
+import About from "./Components/About/About.tsx";
+import Career from "./Components/Career/Career.tsx";
+import Projects from "./Components/Projects/Projects.tsx";
+import Contact from "./Components/Contact/Contact.tsx";
+import Navbar from "./Components/Navbar/Navbar.tsx";
+import Footer from "./Components/Footer/Footer.tsx";
 
-export default function App() {
-  return <LandingPage />;
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="bg-slate-300 min-h-screen">
+        <div className="flex flex-col h-full">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/career" element={<Career />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- PORTFOLIO/src/main.tsx
+++ PORTFOLIO/src/main.tsx
@@ -1,8 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
